03df71cb9b58517bd4a992eba217d7a63598a3aa
shihan-syed <shihan.s@iinerds.com>
2022-09-30 01:07:48 +0530

    css for header added

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -5,8 +5,8 @@ export const Header = () => {
     <div>
       <div>
         <div className='nav' > <img src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png' className='flipkart-logo' />
-        <p> Explore 
-          <span className='plus-color'>pluss</span>
+        <p className='explore '> Explore 
+          <span className='plus-color'> plus</span>
           <img  src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png' alt=''  className='flipkart-plus-logo'/>
           </p>
         </div>  

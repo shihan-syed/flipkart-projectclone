898317ba98f19fef8bb06bcacc08e20ae307d4a3
shihan-syed <shihan.s@iinerds.com>
2022-09-30 02:31:15 +0530

    edit

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -4,13 +4,13 @@ export const Header = () => {
   return (
     <div>
       <div>
-        <div className='nav' > <img src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png' className='flipkart-logo' />
+        <nav className='nav' > <img src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png' className='flipkart-logo' />
         <p className='explore '>Explore 
           <span className='plus-color'> plus</span>
           <img  src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png' alt='image'  className='flipkart-plus-logo'/>
           </p>
         </div>  
-      </div>
+      </nav>
     </div>
   )
 }

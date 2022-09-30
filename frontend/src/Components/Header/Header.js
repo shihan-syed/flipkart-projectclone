fc372a16bf9debdc6f90476ae311445a0fb5ab23
shihan-syed <shihan.s@iinerds.com>
2022-09-30 14:10:02 +0530

    edit

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -6,12 +6,12 @@ export const Header = () =>
    
       <div className='topnav'>
         <div className='logo-area' > <img src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png' className='flipkart-logo' alt="" />
-        <div ><p className='explore '>Explore 
-          <span className='plus-color'> plus</span>
+             <p className='explore '>Explore 
+          <span className='plus-color'> Plus</span>
           <img  src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png' alt='image'  className='flipkart-plus-logo'/>
           </p>
-          </div>
         </div>  
+        <div className=''><input type='text' placeholder='sssssss'></input></div>
       
       </div>
   

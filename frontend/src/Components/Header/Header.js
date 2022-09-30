8fa481ab990d34050858ae6ff648982cc555b295
Jagan2222 <jagan.vjs@iinerds.com>
2022-09-30 10:32:27 +0530

    carousel

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <div>
       <div>
-        <div className='nav' > <img src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png' className='flipkart-logo' />
+        <div className='nav' > <img src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png' className='flipkart-logo' alt='' />
         <p> Explore 
           <span className='plus-color'>pluss</span>
           <img  src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png' alt=''  className='flipkart-plus-logo'/>
